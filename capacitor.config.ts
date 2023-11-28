2713ced3d5ddb7591ddb741db723040b9ea8ad73
Victor Faundez <victor.faundez@impact.cl>
2023-11-27 22:26:08 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Trawen-Live',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
